6ba53e57bf98ab122552efcf76582048fcc6072a
mbelengonzalez168 <138723897+mbelengonzalez168@users.noreply.github.com>
2024-03-28 14:11:41 -0300

    Update entrypoint.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-# Limpiar el espacio de trabajo
+#archivo de punto de entrada para ejecutar un contenedor. Su función principal es configurar el entorno 
+#y ejecutar los comandos necesarios para iniciar la aplicación o los servicios dentro del contenedor. 
+#realiza todos los pasos del pipeline
+
+# CleanWorkspace: Limpiar el espacio de trabajo 
 cleanWs
 
-# Clonar el repositorio de Docker y construir la imagen
+# clonacionImagen: Clonar el repositorio de Docker y construir la imagen
 git clone -b main $REPO_DOCKER $WORKSPACE
 docker buildx build -t $NOMBRE_APELLIDO:$TAG_IMAGEN .
 
-# Dar permisos de ejecución a los scripts en el directorio 'app'
+# Automatización :
+#Dar permisos de ejecución a los scripts en el directorio 'app'
 chmod +x $WORKSPACE/app/*
-
 # Ejecutar el script de clonación
 sh clone.sh $RAMA $REPOSITORIO
-
 # Ejecutar el contenedor Docker con las variables de entorno definidas
 docker run -e RAMA=$RAMA -e REPOSITORIO=$REPOSITORIO -e TAG=$TAG -e NAV=$NAV $NOMBRE_APELLIDO:$TAG_IMAGEN
 
-# Realizar análisis de eficiencia con DIVE
+# Análisis de Eficiencia - DIVE: 
 CI=true dive $NOMBRE_APELLIDO:$TAG_IMAGEN > log.txt
 cat log.txt
 
-# Realizar análisis de archivo Dockerfile con HADOLINT
+# Análisis de archivo DockerFile - HADOLINT. 
 docker run --rm -i hadolint/hadolint:latest < Dockerfile > logfile.txt || true
 cat logfile.txt
 
-# Realizar análisis de vulnerabilidades con TRIVY
+# Análisis de Vulnerabilidades - TRIVY
 trivy image $NOMBRE_APELLIDO:$TAG_IMAGEN > trivy.txt
 cat trivy.txt
 
