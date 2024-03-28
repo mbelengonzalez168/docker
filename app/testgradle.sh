dfe97103a88c15cadb721e6526b165be695fc7b1
mbelengonzalez168 <138723897+mbelengonzalez168@users.noreply.github.com>
2024-03-28 13:50:54 -0300

    Update testgradle.sh

---
+++ app/testgradle.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Obtener los argumentos pasados al script
+TAG=$1
+NAV=$2
+
+# Cambiar al directorio donde se encuentra el proyecto de Gradle
+cd /opt/framework
+
+# Ejecutar las pruebas de Gradle con los argumentos proporcionados
+./gradlew test -Dtag=$TAG -Dnav=$NAV
