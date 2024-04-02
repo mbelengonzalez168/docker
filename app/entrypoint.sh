9726e1e7502ab3ea493b2fd981ab4135a67916df
mbelengonzalez168 <138723897+mbelengonzalez168@users.noreply.github.com>
2024-04-02 10:38:14 -0300

    Update entrypoint.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,12 +3,12 @@ RAMA=$1
 REPOSITORIO=$2
 TAG=$3
 NAV=$4
-chmod 777 /opt/
+sudo chmod 777 /opt/
 cd /opt
 pwd
 ls
-chmod +x clone.sh
-chmod +x testgradle.sh
+sudo chmod +x clone.sh
+sudo chmod +x testgradle.sh
 sh clone.sh ${RAMA} ${REPOSITORIO}
 cd /opt/framework
 cp /opt/testgradle.sh /opt/framework
