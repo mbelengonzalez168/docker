3a8029ca71c47244aa795839c99e81aa986c1e9d
mbelengonzalez168 <138723897+mbelengonzalez168@users.noreply.github.com>
2024-04-02 12:49:39 -0300

    Update testgradle.sh

--- app/testgradle.sh
+++ app/testgradle.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 TAG=$1
 NAV=$2
-
-gradle build --refresh-dependencies
 xvfb-run -a --server-args="-screen 0 1920x1080x24" gradle runWithCucumber -Ptags=$TAG -Pnav=$NAV || true
 ls /opt/framework/results
